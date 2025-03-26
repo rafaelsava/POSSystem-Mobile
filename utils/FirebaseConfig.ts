f499cf0d428ebde5d3adb9ff69a3a7f3f67ef24a
rafaelsava <rafaelsava@unisabana.edu.co>
2025-03-26 13:31:28 -0500

    FirebaseConfig

---
+++ utils/FirebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAkGnIEDt65IftyA7g5QJYyqEERqoHGjo",
+  authDomain: "possystem-mobile.firebaseapp.com",
+  projectId: "possystem-mobile",
+  storageBucket: "possystem-mobile.firebasestorage.app",
+  messagingSenderId: "780292225086",
+  appId: "1:780292225086:web:bfca6c3f6ed5c48a76796f",
+  measurementId: "G-0X8BB7GV6L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
